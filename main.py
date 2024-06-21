6e64f93ab47162098b35da6845e8e28e852d8ba9
Mohammad <roze.sorkh.777@gmail.com>
2024-06-21 12:23:31 +0330

    v 1.1

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import tkinter as tk
-from tkinter import ttk
 from PIL import Image
+from tkinter.filedialog import asksaveasfilename
 
 import qrcode
 import customtkinter as ctk
 
-ctk.set_default_color_theme('dark-blue')
+# Customtkinter Configuration
+# ctk.set_default_color_theme('dark-blue')
 
+IMG_WIDTH = 400
+IMG_HEIGHT = 400
 
 class QrCodeGenerator(ctk.CTk):
     def __init__(self):
         super().__init__(fg_color='white')
-        self.geometry('400x430')
+        self.geometry(f'{IMG_WIDTH+100}x{IMG_HEIGHT+150}')
         self.title('')
         self.iconbitmap('assets/empty.ico')
         self.minsize(width=120, height=0)
 
-        self.top_frame = TopFrame(self, width=400, height=400, fg_color='white')
-        # self.top_frame.pack(expand=True, fill='both')
+        self.top_frame = TopFrame(self, width=IMG_WIDTH, height=IMG_HEIGHT, fg_color='white')
         self.top_frame.place(relx=0.5, rely=0.4, anchor='center', relwidth=0.6, relheight=0.6)
 
         self.bottom_frame = BottomFrame(self,
@@ -25,14 +27,14 @@ class QrCodeGenerator(ctk.CTk):
                                         height=100,
                                         corner_radius=20
                                         )
-        self.bottom_frame.place(relx=0, rely=0.8, relwidth=1, relheight=0.35)
+        self.bottom_frame.place(relx=0, rely=0.8, relwidth=1, relheight=0.25)
 
 
 class TopFrame(ctk.CTkFrame):
     def __init__(self, parent, **kwargs):
         super().__init__(master=parent, **kwargs)
 
-        self.qr_img = ctk.CTkImage(Image.open('assets/Placeholder.png'), size=(300, 300))
+        self.qr_img = ctk.CTkImage(Image.open('assets/Placeholder.png'), size=(IMG_WIDTH, IMG_HEIGHT))
         self.image_label = ctk.CTkLabel(self, image=self.qr_img, text='')
         self.image_label.pack(expand=True, fill='both')
 
@@ -40,28 +42,32 @@ class TopFrame(ctk.CTkFrame):
 class BottomFrame(ctk.CTkFrame):
     def __init__(self, parent, **kwargs):
         super().__init__(master=parent, **kwargs)
+        self.pil_image = None
         self.parent = parent
 
         # Entry box
         self.buttons_frame = ctk.CTkFrame(self, fg_color='#021fb3')
-        self.buttons_frame.place(relx=0.5, rely=0.3, anchor='center')
-        # self.buttons_frame.pack(expand=True, fill='x', anchor='n')
+        self.buttons_frame.place(relx=0.5, rely=0.4, anchor='center')
         self.ent_var = tk.StringVar()
         self.ent_var.trace_add(mode='write',
                                callback=self.create_qr)
         self.entry = ctk.CTkEntry(self.buttons_frame,
-                                  width=180,
+                                  width=200,
                                   fg_color='#2e54e8',
                                   border_width=0,
                                   text_color='white',
-                                  textvariable=self.ent_var
+                                  textvariable=self.ent_var,
                                   )
-        # self.entry.pack(side='left')
-        self.entry.columnconfigure((0, 1, 2), weight=1)
-        self.entry.grid(row=0, column=0, columnspan=2, padx=5)
+        self.entry.columnconfigure((0, 1, 2, 3), weight=1)
+        self.entry.grid(row=0, column=0, columnspan=3, padx=5)
 
-        self.button = ctk.CTkButton(self.buttons_frame, text='Save', fg_color='#2e54e8', width=50)
-        self.button.grid(row=0, column=2, padx=5)
+        self.button = ctk.CTkButton(self.buttons_frame,
+                                    text='Save',
+                                    fg_color='#2e54e8',
+                                    width=80,
+                                    command=self.save_image
+                                    )
+        self.button.grid(row=0, column=3, padx=5)
 
     def create_qr(self, *args):
         qr = qrcode.QRCode(
@@ -79,10 +85,17 @@ class BottomFrame(ctk.CTkFrame):
         qr_image = qr.make_image(fill_color="black", back_color="white")
 
         # Convert qrcode.image.pil.PilImage to PIL.Image.Image
-        pil_image = qr_image.get_image()
+        self.pil_image = qr_image.get_image()
         # img = qrcode.make(self.entry.get()).get_image()
-        self.new_qr = ctk.CTkImage(pil_image, size=(300, 300))
+        self.new_qr = ctk.CTkImage(self.pil_image, size=(IMG_WIDTH, IMG_HEIGHT))
         self.parent.top_frame.image_label.configure(image=self.new_qr)
+
+    def save_image(self):
+        if self.pil_image:
+            img = self.pil_image
+            file_name = asksaveasfilename(title='Save location', defaultextension='.png')
+            if file_name:
+                img.save(file_name)
 
 
 app = QrCodeGenerator()
