d91b7b193cda1376c15a2b77d578e37eff011ff4
Mohammad <roze.sorkh.777@gmail.com>
2024-06-21 16:57:16 +0330

    v 1.3

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ import customtkinter as ctk
 # QR code image size
 IMG_WIDTH = 400
 IMG_HEIGHT = 400
+# Set the appearance to light
+ctk.set_appearance_mode('light')
 
 
 class QrCodeGenerator(ctk.CTk):
     def __init__(self):
-        super().__init__(fg_color='white')
-        self.geometry(f'{IMG_WIDTH+150}x{IMG_HEIGHT+150}')
+        super().__init__(fg_color='white')  # setting the window color
+        # window setup
+        self.geometry(f'{IMG_WIDTH + 150}x{IMG_HEIGHT + 150}')
         self.title('')
         self.iconbitmap('assets/empty.ico')
         self.minsize(width=120, height=0)
@@ -26,22 +29,26 @@ class QrCodeGenerator(ctk.CTk):
                                         height=100,
                                         corner_radius=20
                                         )
-        self.bottom_frame.place(relx=0, rely=0.8, relwidth=1, relheight=0.235, anchor='nw')
+        self.bottom_frame.place(relx=0,
+                                rely=0.8,
+                                relwidth=1,
+                                relheight=0.235,
+                                anchor='nw')
 
 
 class TopFrame(ctk.CTkFrame):
     """Frame which contains qr code image inside a label."""
+
     def __init__(self, parent, **kwargs):
         super().__init__(master=parent, **kwargs)
         # Label which contains generated qr code
-        # An initial placeholder image added for our program start up.
-        qr_img = ctk.CTkImage(Image.open('assets/Placeholder.png'), size=(IMG_WIDTH, IMG_HEIGHT))
-        self.image_label = ctk.CTkLabel(self, image=qr_img, text='')
+        self.image_label = ctk.CTkLabel(self, text='')
         self.image_label.pack(expand=True, fill='both')
 
 
 class BottomFrame(ctk.CTkFrame):
     """Frame which contains Entry box and button."""
+
     def __init__(self, parent, **kwargs):
         super().__init__(master=parent, **kwargs)
         self.pil_image = None  # will contain the pillow Image version.
@@ -65,11 +72,15 @@ class BottomFrame(ctk.CTkFrame):
                                   textvariable=self.ent_var,
                                   )
         self.entry.grid(row=0, column=0, padx=5)
+        # Binding Enter key on keyboard to save function.
+        self.entry.bind('<Return>', self.save_image)
+
 
         # save button
         self.button = ctk.CTkButton(self.buttons_frame,
                                     text='Save',
                                     fg_color='#2e54e8',
+                                    hover_color='#4266f1',
                                     width=80,
                                     command=self.save_image
                                     )
@@ -79,30 +90,32 @@ class BottomFrame(ctk.CTkFrame):
         """Generates A qr code from provided text by user.
             It then updates the Label widget with new image.
         """
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=3,
-        )
+        current_text = self.entry.get()
+        if current_text:  # If text is not empty
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=3,
+            )
+            # Add data to the QR code
+            qr.add_data(current_text)
+            qr.make(fit=True)
 
-        # Add data to the QR code
-        qr.add_data(self.entry.get())
-        qr.make(fit=True)
+            # Create an image from the QR code instance
+            qr_image = qr.make_image(fill_color="black", back_color="white")
 
-        # Create an image from the QR code instance
-        qr_image = qr.make_image(fill_color="black", back_color="white")
+            # Convert qrcode.image.pil.PilImage to PIL.Image.Image
+            self.pil_image = qr_image.get_image().resize((IMG_WIDTH, IMG_HEIGHT))
+            new_qr = ctk.CTkImage(self.pil_image, size=(IMG_WIDTH, IMG_HEIGHT))
+            self.label_widget.configure(image=new_qr)
+        else:  # If no text, remove the last image
+            self.label_widget.configure(image=None)
 
-        # Convert qrcode.image.pil.PilImage to PIL.Image.Image
-        self.pil_image = qr_image.get_image()
-        # img = qrcode.make(self.entry.get()).get_image()
-        new_qr = ctk.CTkImage(self.pil_image, size=(IMG_WIDTH, IMG_HEIGHT))
-        self.label_widget.configure(image=new_qr)
-
-    def save_image(self):
+    def save_image(self, event=None):
         """Prompts user for a file name and then saves the qr image."""
         if self.pil_image:  # Image already generated
-            file_name = asksaveasfilename(title='Save location', defaultextension='.png')
+            file_name = asksaveasfilename(title='Save location', defaultextension='.jpg')
             if file_name:  # If a name has been specified
                 self.pil_image.save(file_name)
 
